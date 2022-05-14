028725778c674bf8e2ae94883e55d057b04997f0
Mamoru <senkinsaha@mail.ru>
2022-05-14 09:24:20 +0400

    Lab5

--- SecuritySystem/SecuritySystemView/FormMain.cs
+++ SecuritySystem/SecuritySystemView/FormMain.cs
@@ -38,7 +38,7 @@ namespace SecuritySystemView
                     dataGridView.Columns[0].Visible = false;
                     dataGridView.Columns[1].Visible = false;
                     dataGridView.Columns[3].Visible = false;
-                    dataGridView.Columns[7].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    //dataGridView.Columns[7].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
                 }
             }
             catch (Exception ex)
